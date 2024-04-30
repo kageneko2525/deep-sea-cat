e7de6fb04ba951a41eb4b8ccff24ea415e85d877
kageneko2525 <146046319+kageneko2525@users.noreply.github.com>
2024-04-30 20:21:42 +0900

    Add files via upload

---
+++ content/js/foam.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    var imageUrl = "images/foam.png";
+
+    function createImage() {
+        var img = new Image();
+        img.src = imageUrl;
+        img.style.position = 'absolute';
+        img.style.bottom = '0px';
+        img.style.left = Math.random() * window.innerWidth + 'px';
+        img.style.width = Math.random() * 3 + 10 + 'px';
+        img.speed = Math.random() * 3 + 2;
+        img.style.zIndex = -1;
+        document.body.appendChild(img);
+        return img;
+    }
+
+    function moveImage(img) {
+        img.style.bottom = parseInt(img.style.bottom) + (img.speed*Math.random()+2) + 'px';
+        if (parseInt(img.style.bottom) > window.innerHeight) {
+            document.body.removeChild(img);
+        } else {
+            img.style.left = parseInt(img.style.left) + Math.sin(new Date().getTime() / 100) * 0.1 + 'px';
+            requestAnimationFrame(function() { moveImage(img); });
+        }
+    }
+
+    function generateImages() {
+        var img = createImage();
+        moveImage(img);
+        setTimeout(generateImages, Math.random() * 2000);
+    }
+
+    generateImages();
+});
